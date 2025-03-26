b6afa79c3756d707313438790e08a9ba9149c005
= <solutions.provider.dev@gmail.com>
2025-03-26 13:10:29 +0530

    Remove comment

--- chatbot/models.py
+++ chatbot/models.py
@@ -13,7 +13,7 @@ class OTP(models.Model):
     def __str__(self):
         return f"{self.email} - {self.otp_code}"
 
-# Add user profile
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     name = models.CharField(max_length=255)
