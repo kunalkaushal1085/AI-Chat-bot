fcd29a03e454b5ee9ff19acef7fbd8c2bf70a9a9
Kunal Kaushal <kunal1085@gmail.com>
2025-06-06 13:20:14 +0530

    Add get user token function in base.py

--- utils/base.py
+++ utils/base.py
@@ -1,8 +1,6 @@
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 import os
-from rest_framework.response import Response
-from rest_framework import status
 
 load_dotenv()
 
@@ -15,15 +13,20 @@ ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
 
 cipher = Fernet(ENCRYPTION_KEY)
 def encrypt_data(data: str) -> str:
-    try:
-        """Encrypt the given data using Fernet."""
-        return cipher.encrypt(data.encode()).decode()
-    except Exception as e:
-        return Response({"status":status.HTTP_400_BAD_REQUEST,"message":str(e)})
+    """Encrypt the given data using Fernet."""
+    return cipher.encrypt(data.encode()).decode()
 
 def decrypt_data(encrypted_data: str) -> str:
+    """Decrypt the given data using Fernet."""
+    return cipher.decrypt(encrypted_data.encode()).decode()
+
+
+def get_user_from_token(request):
+    from rest_framework_simplejwt.authentication import JWTAuthentication
     try:
-        """Decrypt the given data using Fernet."""
-        return cipher.decrypt(encrypted_data.encode()).decode()
+        auth = JWTAuthentication()
+        validated_token = auth.get_validated_token(auth.get_raw_token(request.headers.get("Authorization").split()[1]))
+        user = auth.get_user(validated_token)
+        return user
     except Exception as e:
-        return Response({"status":status.HTTP_400_BAD_REQUEST,"message":str(e)})
+        return None
