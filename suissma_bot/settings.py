c3988b320ef69226c66f3cdf57c69ce080b83ffa
Kunal Kaushal <kunal1085@gmail.com>
2025-07-03 10:18:17 +0530

    Update the media url in url .py file

--- suissma_bot/settings.py
+++ suissma_bot/settings.py
@@ -196,9 +196,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'chatbot', 'static'),
-# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
