7bbd35a988df5cc220474d7c6aac02b0dafdc918
Kunal Kaushal <kunal1085@gmail.com>
2025-07-10 15:47:43 +0530

    update live credentials

--- suissma_bot/settings.py
+++ suissma_bot/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
